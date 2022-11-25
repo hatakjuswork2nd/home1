eb743b4c18864a13abb7a3a8cf08078fafb7d568
DESKTOP-6HGL501\0005 <hata.k.juswork2nd@gmail.com>
2022-11-25 14:48:58 +0900

    1125

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <!--<link rel="stylesheet" href="{{ asset('css/app.css') }}">-->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
